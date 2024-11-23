af2af7c326c3216ae26a2f9b080813a2ce18792a
kusthanapat <102140251+kusthanapat@users.noreply.github.com>
2024-11-24 01:44:11 +0700

    mydurian-master

--- plugins/vue-apexcharts.js
+++ plugins/vue-apexcharts.js
@@ -1,6 +1,5 @@
 export default defineNuxtPlugin((nuxtApp) => {
-    if (process.server) return; // หยุดการโหลด plugin ใน server-side
-  
+    if (process.client) return; // หยุดการโหลด plugin ใน server-side
     import('vue3-apexcharts').then((module) => {
       const VueApexCharts = module.default;
       nuxtApp.vueApp.component('apexchart', VueApexCharts);
